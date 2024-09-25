de10b4acbf194dea790e4425370ec1bda1c1d629
Faig Ibragimov <149930150+wdsunsif@users.noreply.github.com>
2024-09-25 10:44:50 +0400

    Update MainWindow.xaml.cs

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
-namespace TaskManagerClientUI;
+namespace TaskManagerClient;
 public partial class MainWindow : Window
 {
 	public MainWindow()
